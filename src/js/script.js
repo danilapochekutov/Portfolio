c7996ceb0289c110ac39fda86384553fec5026e0
Danila <danya270203@gmail.com>
2023-07-22 23:20:32 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close'),
+    link = document.querySelector('.menu__link');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+link.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+    lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
